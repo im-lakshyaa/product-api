23260680496368c6c65e77cd091068751069cd52
im-lakshyaa <lakshya20042004@gmail.com>
2025-10-30 14:56:42 +0530

    add start script for render deployment

--- api.js
+++ api.js
@@ -4,8 +4,11 @@ import fs from 'fs';
 const PORT = 3000;
 const app = express();
 
-
 app.get('/', (req, res) => {
+  res.send("Welcome to Product API");
+});
+
+app.get('/product', (req, res) => {
   fs.readFile('product.json', 'utf-8', (err, data) => {
     if (err) {
       res.status(500).send("Error reading file");
